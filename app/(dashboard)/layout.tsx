0a5b518b3a76891f3de99bcc32fefcef413fc7a4
HyFy Designs <hyfydesigns2020@gmail.com>
2026-04-09 13:04:16 -0500

    Auto-save: app/(dashboard)/layout.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,8 +1,7 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "@/lib/auth";
-import { DashboardSidebar } from "@/components/dashboard/sidebar";
-import { DashboardHeader } from "@/components/dashboard/header";
+import { DashboardShell } from "@/components/dashboard/shell";
 
 export default async function DashboardLayout({
   children,
@@ -15,12 +14,8 @@ export default async function DashboardLayout({
   }
 
   return (
-    <div className="flex h-screen bg-gray-50 overflow-hidden">
-      <DashboardSidebar />
-      <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
-        <DashboardHeader user={session.user} />
-        <main className="flex-1 overflow-auto">{children}</main>
-      </div>
-    </div>
+    <DashboardShell user={session.user}>
+      {children}
+    </DashboardShell>
   );
 }
