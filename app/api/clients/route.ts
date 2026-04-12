e55c0453f6836d41b54b7951dec0b7fb3aeb1a40
HyFy Designs <hyfydesigns2020@gmail.com>
2026-04-12 09:55:29 -0500

    Auto-save: app/api/clients/route.ts

--- app/api/clients/route.ts
+++ app/api/clients/route.ts
@@ -4,6 +4,7 @@ import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
 import crypto from "crypto";
 import { sendClientInviteEmail } from "@/lib/email";
+import { getEmailBranding } from "@/lib/email-branding";
 import { logActivity } from "@/lib/activity";
 import { getAppUrl } from "@/lib/utils";
 
